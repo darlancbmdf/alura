f930adcb346b45f425206a30b18997186711025a
darlancbmdf <darlan.cbmdf@gmail.com>
2023-06-11 22:57:14 -0300

    alterando nome da pasta Python

--- PYTHON/adivinhacao.py
+++ PYTHON/adivinhacao.py
@@ -6,16 +6,16 @@ print("*********************************")
 
 numero_secreto = round(random.randrange(1, 11))
 total_de_tentativas = 3
-#rodada = 1 #código do while
+# rodada = 1 #código do while
 
-#while rodada <= total_de_tentativas:
+# while rodada <= total_de_tentativas:
 for rodada in range(1,  total_de_tentativas + 1):
     print("tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 10: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
 
-    if(chute < 1 or chute > 10):
+    if chute < 1 or chute > 10:
         print("Você deve digitar um número entre 1 e 10")
         continue
 
@@ -31,6 +31,6 @@ for rodada in range(1,  total_de_tentativas + 1):
             print("Você errou, seu chute foi maior que o número secreto")
         elif (menor):
             print("Você errou, seu chute foi menor que o número secreto")
-    #rodada += 1 #código do while
+    # rodada += 1 #código do while
 
 print("Fim do jogo.")
