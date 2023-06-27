3a592746c19fca9d2b4715e9ea2a421b75af66c7
darlancbmdf <darlan.cbmdf@gmail.com>
2023-06-27 17:15:57 -0300

    validação de CPF e CNPJ

--- PYTHON/brasilidades/main.py
+++ PYTHON/brasilidades/main.py
@@ -1,1 +1,7 @@
-from cpf import Cpf
+from cpf_cnpj import Documento
+
+exemplo_cnpj = '08977914000119'
+exemplo_cpf = '83234853134'
+
+documento = Documento.cria_documento(exemplo_cpf)
+print(documento)
