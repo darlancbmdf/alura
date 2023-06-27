ce3902af8c55e9bfa8d379aaaaf81d639c1d4a86
darlancbmdf <darlan.cbmdf@gmail.com>
2023-06-27 17:19:53 -0300

    teste de  CPF e CNPJ

--- PYTHON/brasilidades/main.py
+++ PYTHON/brasilidades/main.py
@@ -1,7 +1,7 @@
 from cpf_cnpj import Documento
 
 exemplo_cnpj = '08977914000119'
-exemplo_cpf = '83234853134'
+exemplo_cpf = '832348531345'
 
 documento = Documento.cria_documento(exemplo_cpf)
 print(documento)
